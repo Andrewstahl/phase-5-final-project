33ab8e8d75c9beecda119ab18453c6fe5f54aff0
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-20 14:49:19 -0500

    Added route framework and react component hierarchy note

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,49 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Login from "../pages/Login";
 
+/**
+ * App Hierarchy
+ * 
+ * App
+ * ├─── Postings
+ *      ├─── Job Requests
+ *           ├─── Job
+ *           ├─── Job
+ *           └─── Job
+ *      └─── Gigs
+ *           ├─── Gig
+ *           ├─── Gig
+ *           └─── Gig
+ * ├─── Conversations
+ *      ├─── Conversation
+ *           ├─── Message
+ *           ├─── Message
+ *           └─── Message
+ *      ├─── Conversation
+ *      └─── Conversation
+ * ├─── Projects
+ *      ├─── Project
+ *      ├─── Project
+ *      └─── Project
+ * ├─── Search
+ *      ├─── User
+ *      ├─── User
+ *      └─── User
+ * └─── Profile
+ * 
+ */
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch("/me")
-      .then((r) => r.json())
-      .then((user) => setUser(user));
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
   }, []);
 
   function handleLogout() {
@@ -18,22 +51,23 @@ function App() {
       method: "DELETE",
     }).then((r) => {
       if (r.ok) {
-        setUser(null)
+        setUser(null);
       }
-    }); 
+    });
   }
 
-  if (!user) return <Login />
-  // return <Login />
+  if (!user) return <Login />;
 
   return (
     <>
-      <NavBar onLogoutClick={handleLogout}/>
+      <NavBar onLogoutClick={handleLogout} />
       <div className="App">
         <Routes>
-          <Route exact path="/" element={
-            <h1>Page Count: {count}</h1>
-          } />
+          <Route exact path="/" element={<h1>Postings</h1>} />
+          <Route exact path="/conversations" element={<h1>Conversations</h1>} />
+          <Route exact path="/projects" element={<h1>Projects</h1>} />
+          <Route exact path="/search" element={<h1>Search</h1>} />
+          <Route exact path="/profile" element={<h1>Profile</h1>} />
         </Routes>
       </div>
     </>
