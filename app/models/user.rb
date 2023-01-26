10f2822ea851a377bed7673c77b72e7ac45b0d1c
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-26 15:17:43 -0500

    Added functionality to destroy all dependent postings

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_one :buyer, dependent: :destroy
   has_one :freelancer, dependent: :destroy
-  has_many :postings
+  has_many :postings, dependent: :destroy
 
   validates :username, presence: true
   validates :username, uniqueness: true
