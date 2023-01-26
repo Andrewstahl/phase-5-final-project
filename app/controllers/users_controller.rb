c4af2ff1c0dfb5c33d248ee6f9858d6fd0424d73
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-26 11:25:59 -0500

    Minor updates to method syntax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,37 @@
 class UsersController < ApplicationController
-  before_action :current_user, only: [:show, :update, :destroy]
+  # before_action :current_user, only: [:show, :update, :destroy]
+  
   skip_before_action :authorized, only: :create
-
+  
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
+  
   # GET /users
   def index
     render json: User.all
   end
-
+  
   # GET /me
   def show
+    @user = User.find(session[:user_id])
     render json: @user
   end
-
+  
   # POST /users
   def create
     @user = User.create!(user_params)
     session[:user_id] = @user.id
     render json: @user, status: :created
   end
-
+  
   # POST/PATCH 
   def update
+    @user = User.find(session[:user_id])
     @user.update!(user_params)
     render json: @user
   end
-
+  
   def destroy
+    @user = User.find(session[:user_id])
     @user.destroy
     head :no_content
   end
