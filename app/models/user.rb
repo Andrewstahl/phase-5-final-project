fb546258ec8794e8e8f36abe3e5e503709ef9052
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-26 12:20:50 -0500

    Added support for buyer, freelancer, and posting connections

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_one :buyer, dependent: :destroy
+  has_one :freelancer, dependent: :destroy
+  has_many :postings
 
   validates :username, presence: true
   validates :username, uniqueness: true
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
+  has_one :buyer
+  has_one :freelancer
+  has_many :postings
   attributes :id, :username
 end
