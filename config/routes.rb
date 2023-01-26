c5bb1850cc0bdfbc5eb92e6d0edc3ca4de9f9da3
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-26 15:17:12 -0500

    Testing nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   resources :postings
   resources :buyers
   resources :freelancers
-  resources :users
 
   get "/me", to: "users#show"
   post "signup", to: "users#create"
