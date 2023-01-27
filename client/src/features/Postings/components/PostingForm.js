2bd447af3f7d436d92cb935e59403baa14063012
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-27 17:24:14 -0500

    Added functionlity to bring in current posting details if the user selected to edit

--- client/src/features/Postings/components/PostingForm.js
+++ client/src/features/Postings/components/PostingForm.js
@@ -1,27 +1,70 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-export default function PostingForm({ post }) {
+export default function PostingForm({ posting, onSubmit, onCancel }) {
+  const [currentPosting, setCurrentPosting] = useState(() => {
+    if (posting) {
+      return {
+        title: posting.title,
+        description: posting.description,
+        categories: posting.categories,
+        price: parseInt(posting.price),
+        price_unit: posting.price_unit.replace(" ", "_").toLowerCase(),
+      };
+    } else {
+      return {
+        title: "",
+        description: "",
+        categories: "Accounting",
+        price: "0",
+        price_unit: "Hourly",
+      };
+    }
+  });
+
+  function handleChange(e) {
+    const name = e.target.name.replace("-", "_");
+    let value = e.target.value;
+
+    if (name === "price-unit") {
+      value = value.replace(" ", "_").toLowerCase();
+    }
+
+    setCurrentPosting({
+      ...currentPosting,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
     <>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="title">Title</label>
-        <input 
+        <input
           id="title"
           name="title"
           type="text"
+          value={currentPosting.title}
+          onChange={(e) => handleChange(e)}
           placeholder="Enter your title here"
         />
         <label htmlFor="description">Description</label>
-        <textarea 
+        <textarea
           id="description"
           name="description"
+          value={currentPosting.description}
+          onChange={(e) => handleChange(e)}
           placeholder="Enter your description here"
         />
         <label htmlFor="categories">Categories</label>
-        <select 
+        <select
           id="categories"
           name="categories"
+          value={currentPosting.categories}
+          onChange={(e) => handleChange(e)}
         >
           <option value="accounting">Accounting</option>
           <option value="administrative">Administrative</option>
@@ -36,27 +79,37 @@ export default function PostingForm({ post }) {
           <option value="sales">Sales</option>
           <option value="software-development">Software Development</option>
           <option value="therapy">Therapy</option>
+          <option value="web-development">Web Development</option>
           <option value="writing">Writing</option>
         </select>
         <label htmlFor="price">Price</label>
-        <input 
+        <input
           id="price"
           name="price"
           type="number"
+          value={currentPosting.price}
+          onChange={(e) => handleChange(e)}
           placeholder="Please enter your price"
           min="0"
         />
         <label htmlFor="price-unit">Price Unit</label>
-        <select 
+        <select
           id="price-unit"
           name="price-unit"
+          value={currentPosting.price_unit}
+          onChange={(e) => handleChange(e)}
         >
           <option value="flat_amount">Flat Amount</option>
           <option value="hourly">Hourly</option>
           <option value="daily">Daily</option>
         </select>
+        <div className="form-action-buttons">
+          <input type="submit" value="Submit" />
+          <button className="cancel" onClick={onCancel}>
+            Cancel
+          </button>
+        </div>
       </form>
     </>
-  )
-
+  );
 }
