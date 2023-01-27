88c5590ddf4235afc362f381a75058316eb30bf4
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-27 17:23:35 -0500

    Added onEdit function to handle when a user is editing the selected posting

--- client/src/features/Postings/components/PostingList.js
+++ client/src/features/Postings/components/PostingList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Posting from "./Posting";
 
-export default function PostingList({ user }) {
+export default function PostingList({ user, onEdit }) {
   const postingElements = user.postings.map((posting) => {
-    return <Posting key={posting.id} posting={posting} />;
+    return <Posting key={posting.id} posting={posting} onEdit={onEdit}/>;
   });
 
   return postingElements;
