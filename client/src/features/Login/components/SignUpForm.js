7f7a697d1196513a5f00b70b6f8b08b606861491
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-27 11:59:51 -0500

    Changed structure of react application and changed import statements to accommodate new file locations

---
+++ client/src/features/Login/components/SignUpForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import Error from "../../../styles/Error";
+
+function SignUpForm({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors([]);
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "CONTENT-TYPE": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        password_confirmation: passwordConfirmation,
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+        // r.json().then((err) => console.log(err));
+      }
+    });
+  }
+
+  return (
+    <>
+      <div>
+        <h1 className="login-page-header">Sign Up Now</h1>
+      </div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            autoComplete="off"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="off"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password-confirmation">Password Confirmation</label>
+          <input
+            type="password"
+            id="password-confirmation"
+            name="password-confirmation"
+            autoComplete="off"
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
+        <div>
+          {errors.map((error) => (
+            <Error key={error} error={error}></Error>
+          ))}
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default SignUpForm;
---
+++ client/src/features/Postings/components/PostingForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default function PostingForm({ post }) {
+
+  return (
+    <>
+      <form>
+        <label htmlFor="title">Title</label>
+        <input 
+          id="title"
+          name="title"
+          type="text"
+          placeholder="Enter your title here"
+        />
+        <label htmlFor="description">Description</label>
+        <textarea 
+          id="description"
+          name="description"
+          placeholder="Enter your description here"
+        />
+        <label htmlFor="categories">Categories</label>
+        <select 
+          id="categories"
+          name="categories"
+        >
+          <option value="accounting">Accounting</option>
+          <option value="administrative">Administrative</option>
+          <option value="bookkeeping">Bookkeeping</option>
+          <option value="data-entry">Data Entry</option>
+          <option value="education">Education</option>
+          <option value="graphic-design">Graphic Design</option>
+          <option value="information-technology">Information Technology</option>
+          <option value="marketing">Marketing</option>
+          <option value="project-management">Project Management</option>
+          <option value="recruiting">Recruiting</option>
+          <option value="sales">Sales</option>
+          <option value="software-development">Software Development</option>
+          <option value="therapy">Therapy</option>
+          <option value="writing">Writing</option>
+        </select>
+        <label htmlFor="price">Price</label>
+        <input 
+          id="price"
+          name="price"
+          type="number"
+          placeholder="Please enter your price"
+          min="0"
+        />
+        <label htmlFor="price-unit">Price Unit</label>
+        <select 
+          id="price-unit"
+          name="price-unit"
+        >
+          <option value="flat_amount">Flat Amount</option>
+          <option value="hourly">Hourly</option>
+          <option value="daily">Daily</option>
+        </select>
+      </form>
+    </>
+  )
+
+}
