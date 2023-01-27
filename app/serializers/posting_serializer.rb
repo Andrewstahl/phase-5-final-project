60c45420e108b2b632580921c628f40454b9904a
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-27 11:57:57 -0500

    Changed 'project_type' to 'posting_type'

--- app/serializers/posting_serializer.rb
+++ app/serializers/posting_serializer.rb
@@ -1,4 +1,4 @@
 class PostingSerializer < ActiveModel::Serializer
   belongs_to :user
-  attributes :id, :title, :description, :categories, :price, :price_unit, :project_type
+  attributes :id, :title, :description, :categories, :price, :price_unit, :posting_type
 end
