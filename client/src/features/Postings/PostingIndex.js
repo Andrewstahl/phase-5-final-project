5751f2f816a9dc59c366a286029c87e1b33f8748
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-28 11:59:24 -0500

    renamed index file on Postings feature page for easier troubleshooting

--- client/src/features/Postings/PostingIndex.js
+++ client/src/features/Postings/PostingIndex.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import PostingList from "./components/PostingList";
 import PostingForm from "./components/PostingForm";
+import { useSystemMode } from "../../SystemModeContext";
 import "./assets/postings.css";
 
 export default function PostingsIndex({ user }) {
   const [showPostingForm, setShowPostingForm] = useState(false);
   const [currentPosting, setCurrentPosting] = useState(null);
+
+  const systemMode = useSystemMode()
 
   function handleEditClick(selectedPosting) {
     setShowPostingForm(true);
@@ -18,17 +21,21 @@ export default function PostingsIndex({ user }) {
   }
 
   function handleCancel() {
-    setShowPostingForm(false)
-    setCurrentPosting(null)
+    setShowPostingForm(false);
+    setCurrentPosting(null);
   }
-
+  
   return (
     <>
       <h1 className="page-header">Postings</h1>
-      <button className="posting-form-button" onClick={togglePostingForm}>
-        Show Posting Form
-      </button>
-      {showPostingForm ? <PostingForm posting={currentPosting} onCancel={handleCancel} /> : null}
+      <div className="posting-form-button-div">
+        <button className={`posting-form-button color-${systemMode.toLowerCase()}`} onClick={togglePostingForm}>
+          Show Posting Form
+        </button>
+      </div>
+      {showPostingForm ? (
+        <PostingForm posting={currentPosting} onCancel={handleCancel} />
+      ) : null}
       <PostingList user={user} onEdit={handleEditClick} />
     </>
   );
