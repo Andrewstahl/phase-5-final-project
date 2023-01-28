aaf6fb8f3d1fd14437514905b6b3c7c21771c054
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-28 12:06:10 -0500

    Adjusted import statement to accommodate PostingsIndex new name

--- client/src/pages/Postings.js
+++ client/src/pages/Postings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PostingsIndex from "../features/postings";
+import PostingsIndex from "../features/postings/PostingIndex";
 
 export default function Postings({ user }) {
   return <PostingsIndex user={user} />;
