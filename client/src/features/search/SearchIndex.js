e0417e7ed6522f75cae777299fdd820bc6e33378
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-28 16:09:00 -0500

    Basic structure created with header, search, and result list

--- client/src/features/search/SearchIndex.js
+++ client/src/features/search/SearchIndex.js
@@ -1,10 +1,17 @@
 import React from "react";
+import SearchFilter from "./components/SearchFilter";
+import SearchResultList from "./components/SearchResultList";
 
-export default function SearchIndex() {
-
+export default function SearchIndex({ user }) {
   return (
     <>
-      
+      <h1 className="page-header">Search</h1>
+      <div className="search-filter">
+        <SearchFilter />
+      </div>
+      <div className="search-results">
+        <SearchResultList user={user} />
+      </div>
     </>
-  )
+  );
 }
