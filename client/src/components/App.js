a89e2ff6c72f086b0656836f246c40abc74ee934
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-28 12:20:31 -0500

    Added search component to file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Login from "../pages/Login";
 import Postings from "../pages/Postings";
+import Search from "../pages/Search";
+
 import { UserProvider } from "../UserContext";
 import { SystemModeProvider } from "../SystemModeContext";
 
@@ -84,7 +86,7 @@ function App() {
             <Routes>
               {/* <Route exact path={`/postings/${siteMode}`} element={<Postings user={user} />} /> */}
               <Route exact path="/postings" element={<Postings user={user} />} />
-              <Route exact path="/search" element={<h1>Search</h1>} />
+              <Route exact path="/search" element={<Search user={user}/>} />
               <Route exact path="/conversations" element={<h1>Conversations</h1>} />
               <Route exact path="/projects" element={<h1>Projects</h1>} />
               <Route exact path="/profile" element={<h1>Profile</h1>} />
