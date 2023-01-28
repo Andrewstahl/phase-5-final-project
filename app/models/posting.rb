fdd855fc2c00534192df81a7c871869c81850026
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-28 10:55:11 -0500

    Added scopes for buyer/freelancer postings and validations for posting attributes

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,5 +1,15 @@
 class Posting < ApplicationRecord
   belongs_to :user
+  scope :freelancer_postings, -> { where(posting_type: "Freelancer") }
+  scope :buyer_postings, -> { where(posting_type: "Buyer") }
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :price_unit, presence: true
+  validates :posting_type, presence: true
+
+  validates :posting_type, inclusion: { in: ["Freelancer", "Buyer"] }
 
   serialize :categories, Array
 end
