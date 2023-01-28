0a35663613bb55dbf136c358b84eb48e11487c6f
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-28 17:58:42 -0500

    Simplified structure by abstracting out filtering functionality and result formatting/structure

--- client/src/features/search/components/SearchResultList.js
+++ client/src/features/search/components/SearchResultList.js
@@ -1,2 +1,11 @@
 import React from "react";
+import SearchResult from "./SearchResult";
 
+export default function SearchResultList({ user, results }) {
+  // Filters out the current user so they don't show up on the report
+  const searchResults = results.map((result) => {
+    return <SearchResult key={result.id} result={result} />;
+  });
+
+  return <div className="result__list__div">{searchResults}</div>;
+}
