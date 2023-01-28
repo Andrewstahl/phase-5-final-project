b2670c2b47e1434c7a1fe6307b3a5b59cda09ca5
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-28 11:13:13 -0500

    Altered order of import statements

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './assets/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import './assets/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
