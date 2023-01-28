40f560009571a5946ebc823f261841770e13533c
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-28 17:55:32 -0500

    Added useEffect for pulling in user results and filtering for search functionality

--- client/src/features/search/SearchIndex.js
+++ client/src/features/search/SearchIndex.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchFilter from "./components/SearchFilter";
 import SearchResultList from "./components/SearchResultList";
+import "./assets/search.css"
 
 export default function SearchIndex({ user }) {
+  const [results, setResults] = useState([]);
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("/users").then((r) => {
+      if (r.ok) {
+        r.json().then((results) => setResults(results));
+      }
+    });
+  }, []);
+
+  function handleSearch(searchInput) {
+    setSearch(searchInput)
+  }
+  const filteredResults = results.filter((result) => result.username.includes(search))
+
   return (
     <>
       <h1 className="page-header">Search</h1>
       <div className="search-filter">
-        <SearchFilter />
+        <SearchFilter onSearch={handleSearch}/>
       </div>
       <div className="search-results">
-        <SearchResultList user={user} />
+        <SearchResultList user={user} results={filteredResults}/>
       </div>
     </>
   );
