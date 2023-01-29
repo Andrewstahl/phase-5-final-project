926f56436d29a38bea047c318bef8507a42ac642
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-29 15:58:48 -0500

    Added created_at and updated_at attributes to pull through into the message

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,4 +1,4 @@
 class MessageSerializer < ActiveModel::Serializer
   # belongs_to :conversation
-  attributes :id, :conversation_id, :body, :sender
+  attributes :id, :conversation_id, :body, :sender, :created_at, :updated_at
 end
