75887ecaa91b02698e841336a6f017c84f106f70
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-29 11:00:23 -0500

    Remove unnecessary receiver_id column from Messages resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_131511) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_155844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,7 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_131511) do
   create_table "messages", force: :cascade do |t|
     t.bigint "conversation_id", null: false
     t.text "body"
-    t.integer "receiver_id"
     t.integer "sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
