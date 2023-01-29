0901fb25a40c40d3817976bf75c682ad1ef36ab9
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-29 14:17:17 -0500

    Changed structure of Message database to accommodate usernames instead of user ids

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_163008) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_180642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,9 +39,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_163008) do
   create_table "messages", force: :cascade do |t|
     t.bigint "conversation_id", null: false
     t.text "body"
-    t.integer "sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sender"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
   end
 
