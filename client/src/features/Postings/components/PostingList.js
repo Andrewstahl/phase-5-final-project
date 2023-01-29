36ec1fdc80b41cedc06aec17fbe88555a3544e84
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-29 07:43:19 -0500

    Changed structure to add wrapper div around entire list

--- client/src/features/Postings/components/PostingList.js
+++ client/src/features/Postings/components/PostingList.js
@@ -3,8 +3,8 @@ import Posting from "./Posting";
 
 export default function PostingList({ user, onEdit }) {
   const postingElements = user.postings.map((posting) => {
-    return <Posting key={posting.id} posting={posting} onEdit={onEdit}/>;
+    return <Posting key={posting.id} posting={posting} onEdit={onEdit} />;
   });
 
-  return postingElements;
+  return <div className="posting__list__div">{postingElements}</div>;
 }
