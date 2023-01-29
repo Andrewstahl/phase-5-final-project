5ad4f6eabda51f4f9e608b0f4050548224a23e62
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-29 07:43:54 -0500

    Removed unnecessary comment

--- client/src/features/search/components/SearchResultList.js
+++ client/src/features/search/components/SearchResultList.js
@@ -2,7 +2,6 @@ import React from "react";
 import SearchResult from "./SearchResult";
 
 export default function SearchResultList({ user, results }) {
-  // Filters out the current user so they don't show up on the report
   const searchResults = results.map((result) => {
     return <SearchResult key={result.id} result={result} />;
   });
