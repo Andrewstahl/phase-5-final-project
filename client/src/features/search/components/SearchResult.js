9fc95f283acd795730eceadb8ec1b8c4358cad79
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-31 22:24:29 -0500

    Simplified page logic by combining multiple loops

--- client/src/features/search/components/SearchResult.js
+++ client/src/features/search/components/SearchResult.js
@@ -1,36 +1,26 @@
 import React from "react";
+import SearchResultList from "./SearchResultList";
 
 export default function SearchResult({ result }) {
-  
-  const buyerPostings = result.postings.filter(
-    (posting) => posting.posting_type === "Freelancer"
-  );
-  const freelancerPostings = result.postings.filter(
-    (posting) => posting.posting_type === "Buyer"
-  );
-  let buyerCategories = [];
   let freelancerCategories = [];
+  let freelancerPostings = [];
+  let buyerCategories = [];
+  let buyerPostings = [];
 
-  if (buyerPostings.length > 0) {
-    buyerPostings.map((posting) => {
-      return posting.categories.map((category) =>
-        buyerCategories.push(category)
-      );
-    });
-  }
-  
-  if (freelancerPostings.length > 0) {
-    freelancerPostings.map((posting) => {
-      return posting.categories.map((category) =>
-        freelancerCategories.push(category)
-      );
-    });
-  }
-  
+  result.postings.forEach((posting) => {
+    if (posting.posting_type === "Freelancer") {
+      freelancerPostings.push(posting);
+      freelancerCategories.push(...posting.categories);
+    } else {
+      buyerPostings.push(posting);
+      buyerCategories.push(...posting.categories);
+    }
+  });
+
   return (
     <div className="result__div form-formatting">
-      <div className="result__header">
-        <h2 className="result__header__text">{result.username}</h2>
+      <div>
+        <h2 className="result__header-text">{result.username}</h2>
       </div>
       <div className="result__body">
         <h4>
