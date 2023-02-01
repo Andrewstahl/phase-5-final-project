4bc8ffad2b8a2b9dfef06f4988151472ab832c45
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-31 23:19:39 -0500

    Workshopping context for grabbing all users

--- client/src/utils/useUsers.js
+++ client/src/utils/useUsers.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 export default function useUsers() {
   const [users, setUsers] = useState([]);
+  
   useEffect(() => {
     fetch("/users").then((r) => {
       if (r.ok) {
