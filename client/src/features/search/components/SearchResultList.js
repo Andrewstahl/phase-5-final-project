32af3ea98e21f42acc11ab9ec4108cbb122f7207
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-31 22:25:17 -0500

    Renaming classes for CSS best practices

--- client/src/features/search/components/SearchResultList.js
+++ client/src/features/search/components/SearchResultList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import SearchResult from "./SearchResult";
 
-export default function SearchResultList({ user, results }) {
+export default function SearchResultList({ results }) {
   const searchResults = results.map((result) => {
     return <SearchResult key={result.id} result={result} />;
   });
 
-  return <div className="result__list__div">{searchResults}</div>;
+  return <div className="search-results-list__div">{searchResults}</div>;
 }
