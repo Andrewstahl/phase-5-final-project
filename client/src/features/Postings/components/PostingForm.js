bc27a86970087f6637fe3a5c49abd90230b33c95
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-31 20:14:05 -0500

    Syntax restructuring, separated categories into separate state variable

--- client/src/features/Postings/components/PostingForm.js
+++ client/src/features/Postings/components/PostingForm.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { useSystemMode } from "../../../SystemModeContext";
+import voca from "voca";
 
 export default function PostingForm({ posting, onSubmit, onCancel }) {
-  const systemMode = useSystemMode()
+  const systemMode = useSystemMode();
 
   const [currentPosting, setCurrentPosting] = useState(() => {
     if (posting) {
       return {
         title: posting.title,
         description: posting.description,
-        categories: posting.categories,
         price: parseInt(posting.price),
         price_unit: posting.price_unit.replace(" ", "_").toLowerCase(),
       };
@@ -22,6 +22,13 @@ export default function PostingForm({ posting, onSubmit, onCancel }) {
         price_unit: "Hourly",
       };
     }
+  });
+
+  const [categories, setCategories] = useState(() => {
+    if (posting) {
+      return [...posting.categories];
+    }
+    return [];
   });
 
   function handleChange(e) {
@@ -38,8 +45,24 @@ export default function PostingForm({ posting, onSubmit, onCancel }) {
     });
   }
 
+  function handleCategoryUpdate(e) {
+    // console.log(voca.titleCase(e.target.value.replace("-", " ")));
+    const category = voca.titleCase(e.target.value.replace("-", " "));
+    const index = categories.indexOf(category);
+    if (index > -1) {
+      setCategories(
+        categories
+          .filter((selectedCategory) => selectedCategory !== category)
+          .sort()
+      );
+    } else {
+      setCategories([...categories, category].sort());
+    }
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+    onSubmit({ ...currentPosting, categories: [...categories] });
   }
 
   return (
@@ -63,28 +86,47 @@ export default function PostingForm({ posting, onSubmit, onCancel }) {
           placeholder="Enter your description here"
         />
         <label htmlFor="categories">Categories</label>
-        <select
-          id="categories"
-          name="categories"
-          value={currentPosting.categories}
-          onChange={(e) => handleChange(e)}
-        >
-          <option value="accounting">Accounting</option>
-          <option value="administrative">Administrative</option>
-          <option value="bookkeeping">Bookkeeping</option>
-          <option value="data-entry">Data Entry</option>
-          <option value="education">Education</option>
-          <option value="graphic-design">Graphic Design</option>
-          <option value="information-technology">Information Technology</option>
-          <option value="marketing">Marketing</option>
-          <option value="project-management">Project Management</option>
-          <option value="recruiting">Recruiting</option>
-          <option value="sales">Sales</option>
-          <option value="software-development">Software Development</option>
-          <option value="therapy">Therapy</option>
-          <option value="web-development">Web Development</option>
-          <option value="writing">Writing</option>
-        </select>
+        <div className="posting-form__select-dropdown-div">
+          <select
+            id="categories"
+            name="categories"
+            className="posting-form__select-dropdown-field"
+            value={currentPosting.categories}
+            onChange={(e) => handleCategoryUpdate(e)}
+          >
+            <option value="please-choose">--Choose a Category--</option>
+            <option value="accounting">Accounting</option>
+            <option value="administrative">Administrative</option>
+            <option value="bookkeeping">Bookkeeping</option>
+            <option value="data-entry">Data Entry</option>
+            <option value="education">Education</option>
+            <option value="graphic-design">Graphic Design</option>
+            <option value="information-technology">
+              Information Technology
+            </option>
+            <option value="marketing">Marketing</option>
+            <option value="project-management">Project Management</option>
+            <option value="recruiting">Recruiting</option>
+            <option value="sales">Sales</option>
+            <option value="software-development">Software Development</option>
+            <option value="therapy">Therapy</option>
+            <option value="web-development">Web Development</option>
+            <option value="writing">Writing</option>
+          </select>
+          {/* This button will allow you to remove all attached categories */}
+          <button
+            className="posting-form__select-dropdown-button clear"
+            onClick={(e) => {
+              e.preventDefault();
+              setCategories([]);
+            }}
+          >
+            X
+          </button>
+        </div>
+        <p className="posting-form__categories-paragraph">
+          Current Categories: {categories.join(" • ")}
+        </p>
         <label htmlFor="price">Price</label>
         <input
           id="price"
@@ -107,7 +149,11 @@ export default function PostingForm({ posting, onSubmit, onCancel }) {
           <option value="daily">Daily</option>
         </select>
         <div className="form-action-buttons">
-          <input type="submit" value="Submit" className={`colors-${systemMode.toLowerCase()}`}/>
+          <input
+            type="submit"
+            value="Submit"
+            className={`colors-${systemMode.toLowerCase()}`}
+          />
           <button className="cancel" onClick={onCancel}>
             Cancel
           </button>
