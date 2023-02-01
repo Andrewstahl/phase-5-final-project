bb1d0f29eef1a91e44f963be37ef63809bf695a8
Andrew Stahl <andrewstahl96@gmail.com>
2023-01-31 22:22:48 -0500

    Cleaned up syntax; simplified structure by leveraging components for formatting

--- client/src/features/search/SearchIndex.js
+++ client/src/features/search/SearchIndex.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import SearchFilter from "./components/SearchFilter";
 import SearchResultList from "./components/SearchResultList";
-import "./assets/search.css"
+import "./assets/search.css";
+import { useUsers } from "../../UserContext";
 
 export default function SearchIndex({ user }) {
   const [results, setResults] = useState([]);
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
 
+  setResults(useUsers())
+  
   useEffect(() => {
     fetch("/users").then((r) => {
       if (r.ok) {
@@ -16,23 +19,21 @@ export default function SearchIndex({ user }) {
   }, []);
 
   function handleSearch(searchInput) {
-    setSearch(searchInput)
+    setSearch(searchInput);
   }
 
   // Possibly add in functionality to filter out own user profile
   // const searchResults = results.filter((result) => result.id !== user.id);
 
-  const filteredResults = results.filter((result) => result.username.includes(search))
+  const filteredResults = results.filter((result) =>
+    result.username.includes(search)
+  );
 
   return (
     <>
       <h1 className="page-header">Search</h1>
-      <div className="search-filter">
-        <SearchFilter onSearch={handleSearch}/>
-      </div>
-      <div className="search-results">
-        <SearchResultList user={user} results={filteredResults}/>
-      </div>
+      <SearchFilter onSearch={handleSearch} />
+      <SearchResultList results={filteredResults} />
     </>
   );
 }
