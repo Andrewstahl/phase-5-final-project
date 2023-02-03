fb273166f4d66e9b504796f5887fee7eafb58cff
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-02 21:04:44 -0500

    Added client-facing folder for my project page

---
+++ client/src/features/projects/ProjectsIndex.js
@@ -0,0 +1,11 @@
+import React from "react";
+import toast, { Toaster } from "react-hot-toast";
+
+export default function ProjectsIndex() {
+
+  return (
+    <>
+      <h1 className="page-header">Your Projects</h1>
+    </>
+  )
+}
