1142833b6a476c64f04d62ad71164adfc76cfcb4
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-02 20:36:09 -0500

    Added all CRUD action methods and error handling responses

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show update destroy ]
+  before_action :get_user, only: [:update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /projects
   def index
@@ -15,27 +18,29 @@ class ProjectsController < ApplicationController
 
   # POST /projects
   def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      render json: @project, status: :created, location: @project
-    else
-      render json: @project.errors, status: :unprocessable_entity
-    end
+    @project = Project.create!(project_params)
+    render json: @project, status: :created, location: @project
   end
 
   # PATCH/PUT /projects/1
   def update
-    if @project.update(project_params)
+    if [@project.freelancer_id, @project.buyer_id].include? @user.id
+      @project.update!(project_params)
       render json: @project
     else
-      render json: @project.errors, status: :unprocessable_entity
+      render_not_authorized_response
     end
   end
 
   # DELETE /projects/1
   def destroy
     @project.destroy
+    if [@project.freelancer_id, @project.buyer_id].include? @user.id
+      @project.destroy
+      head :no_content
+    else
+      render_not_authorized_response
+    end
   end
 
   private
@@ -44,8 +49,24 @@ class ProjectsController < ApplicationController
       @project = Project.find(params[:id])
     end
 
+    def get_user
+      @user = User.find(session[:user_id])
+    end
+
     # Only allow a list of trusted parameters through.
     def project_params
-      params.fetch(:project, {})
+      params.require(:project).permit(:freelancer_id, :buyer_id, :posting_id, :due_date, :cost)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  
+    def render_not_found_response
+      render json: { errors: ["Project not found"] }, status: :not_found
+    end
+  
+    def render_not_authorized_response
+      render json: { errors: "You cannot make a change to a project that does not belong to your account" }, status: :unauthorized
     end
 end
