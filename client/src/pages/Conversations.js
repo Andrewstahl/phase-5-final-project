9f9ea62653df539964ddb91b396d2b0d02aea018
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-04 01:36:22 -0500

    Removed unnecessary user import

--- client/src/pages/Conversations.js
+++ client/src/pages/Conversations.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ConversationsIndex from "../features/conversations/ConversationsIndex";
 
-export default function Conversations({ user }) {
-  return <ConversationsIndex user={user} />;
+export default function Conversations() {
+  return <ConversationsIndex />;
 }
