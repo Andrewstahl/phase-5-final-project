cbf920a63fbb427a907e26e2766b57d92681d0f7
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-04 07:51:31 -0500

    Adjusted attributes

--- app/serializers/freelancer_serializer.rb
+++ app/serializers/freelancer_serializer.rb
@@ -2,5 +2,5 @@ class FreelancerSerializer < ActiveModel::Serializer
   belongs_to :user
   has_many :postings, through: :user
   has_many :projects
-  attributes :id, :user_id, :rating
+  attributes :id, :user_id, :rating, :projects
 end
