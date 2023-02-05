1b64bcc88d9f9c3a71aeffe5e13eda378d16d51d
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-05 16:03:45 -0700

    Simplified login page to only import the Login Index file

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,46 +1,6 @@
-import React, { useState } from "react";
-import SignUpForm from "../features/login/components/SignUpForm";
-import LoginForm from "../features/login/components/LoginForm";
-import "../features/login/assets/login.css";
+import React from "react";
+import LoginIndex from "../features/login/LoginIndex";
 
-function Login({ onLogin }) {
-  const [showLogin, setShowLogin] = useState(true);
-
-  return (
-    <div className="login-page-container">
-      {showLogin ? (
-        <>
-          <LoginForm onLogin={onLogin} />
-          <div className="login__signup__switch__container">
-            <h3>
-              <span>Don't Have an Account?</span>
-            </h3>
-            <button
-              className="login__signup__switch__button"
-              onClick={() => setShowLogin(false)}
-            >
-              Signup
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <SignUpForm onLogin={onLogin} />
-          <div className="login__signup__switch__container">
-            <h3>
-              <span>Already Have an Account?</span>
-            </h3>
-            <button
-              className="login__signup__switch__button"
-              onClick={() => setShowLogin(true)}
-            >
-              Login
-            </button>
-          </div>
-        </>
-      )}
-    </div>
-  );
+export default function Login({ onLogin }) {
+  return <LoginIndex onLogin={onLogin} />;
 }
-
-export default Login;
