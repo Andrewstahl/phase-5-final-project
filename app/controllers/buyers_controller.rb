b043a1a5cececc3f49e5f7faafd10e11c3856e73
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-06 06:10:25 -0700

    Added index and show methods for buyers and freelancers since we're only concerned with reading this data

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,21 @@
 class BuyersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  skip_before_action :authorized
+  
+  # GET /buyers
+  def index
+    render json: Buyer.all
+  end
+  
+  # GET /buyer/:id
+  def show
+    @buyer = Buyer.find(params[:id])
+    render json: @buyer
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { errors: ["Buyer not found"] }, status: :not_found
+  end
 end
--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -1,2 +1,22 @@
 class FreelancersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  skip_before_action :authorized
+  
+  # GET /freelancers
+  def index
+    render json: Freelancer.all
+  end
+  
+  # GET /freelancer/:id
+  def show
+    @freelancer = Freelancer.find(params[:id])
+    render json: @freelancer
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { errors: ["Freelancer not found"] }, status: :not_found
+  end
+
 end
