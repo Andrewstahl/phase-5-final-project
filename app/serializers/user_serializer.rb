57e2b9971048741dc5e2cb70afa0b6deff22b6b9
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-06 06:13:14 -0700

    Added custom methods to pull in the freelancer and buyer IDs instead of linked them with has_one methods

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  has_one :buyer
-  has_one :freelancer
+  # has_one :buyer
+  # has_one :freelancer
   has_many :postings
-  attributes :id, :username, :created_at
+
+  attributes :id, :username, :created_at, :freelancer_id, :buyer_id
+  
+  def freelancer_id
+    return object.freelancer.id
+  end
+  
+  def buyer_id
+    return object.buyer.id
+  end
+
 end
