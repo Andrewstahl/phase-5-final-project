986213fb5d101faeca504e3ab93f07d68d1881f4
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-06 06:16:19 -0700

    Setting framework for building out these components

--- client/src/features/projects/ProjectsIndex.js
+++ client/src/features/projects/ProjectsIndex.js
@@ -1,11 +1,23 @@
 import React from "react";
 import toast, { Toaster } from "react-hot-toast";
+import { useUser } from "../../components/App";
+import { useSystemMode } from "../../SystemModeContext";
+import ActiveProjectsList from "./components/ActiveProjectsList";
+import PreviousProjectsList from "./components/PreviousProjectsList";
+import ProjectsForm from "./components/ProjectsForm";
 
 export default function ProjectsIndex() {
 
+  const user = useUser();
+  const systemMode = useSystemMode();
+
+  console.log(user)
   return (
     <>
-      <h1 className="page-header">Your Projects</h1>
+      <h1 className="page-header">Your {systemMode} Projects</h1>
+      <ProjectsForm />
+      <ActiveProjectsList projects={""} />
+      <PreviousProjectsList projects={""} />
     </>
   )
 }
