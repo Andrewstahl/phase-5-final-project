f822de8013c7a1224b019f26c2980744326c7775
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-06 06:09:53 -0700

    Added framework for different components on the projects page

---
+++ client/src/features/projects/components/ActiveProjectsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ActiveProjectsList({ projects }) {
+
+  return (
+    <>
+      <div>
+        <h3 class="text-center">Active Projects</h3>
+        <hr></hr>
+      </div>
+      <div>
+        
+      </div>
+    </>
+  )
+}
---
+++ client/src/features/projects/components/PreviousProjectsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function PreviousProjectsList({ projects }) {
+
+  return (
+    <>
+      <div>
+        <h3 class="text-center">Previous Projects</h3>
+        <hr></hr>
+      </div>
+      <div>
+        
+      </div>
+    </>
+  )
+}
