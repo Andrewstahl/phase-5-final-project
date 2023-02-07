2c3c2e97ed5f9e39b641177c61f6c9e4ae950bea
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-07 05:48:17 -0700

    Changed page sub-header formatting

--- client/src/features/projects/components/ActiveProjectsList.js
+++ client/src/features/projects/components/ActiveProjectsList.js
@@ -5,7 +5,7 @@ export default function ActiveProjectsList({ projects }) {
   return (
     <>
       <div>
-        <h3 class="text-center">Active Projects</h3>
+        <h3 class="text-center mt-5">Active Projects</h3>
         <hr></hr>
       </div>
       <div>
