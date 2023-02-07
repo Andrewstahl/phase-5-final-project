c2434f9ed052e4326380c7959a7b4b417a279233
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-07 05:48:22 -0700

    Changed page sub-header formatting

--- client/src/features/projects/components/PreviousProjectsList.js
+++ client/src/features/projects/components/PreviousProjectsList.js
@@ -5,7 +5,7 @@ export default function PreviousProjectsList({ projects }) {
   return (
     <>
       <div>
-        <h3 class="text-center">Previous Projects</h3>
+        <h3 class="text-center mt-5">Previous Projects</h3>
         <hr></hr>
       </div>
       <div>
