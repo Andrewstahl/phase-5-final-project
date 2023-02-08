a59567a1adfa41d939f1f910c1ff7c75704d696a
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-07 21:26:24 -0700

    Added additional validations for cost and due date

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   belongs_to :freelancer
   belongs_to :buyer
   belongs_to :posting
+
+  validates :cost, presence: true
+  validates :cost, numericality: { greater_than: 0 }
+  validates :due_date, presence: true
 end
