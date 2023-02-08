eebfd0b37af21c90fdad1113ee0aead2ae3aa89e
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-08 09:29:51 -0700

    Removed unnecessary controllers

--- app/controllers/buyer_postings_controller.rb
+++
@@ -1,6 +0,0 @@
-class BuyerPostingsController < ApplicationController
-  def index
-    @postings = Posting.buyer_postings
-    render json: @postings
-  end
-end
--- app/controllers/freelancer_postings_controller.rb
+++
@@ -1,6 +0,0 @@
-class FreelancerPostingsController < ApplicationController
-  def index
-    @postings = Posting.freelancer_postings
-    render json: @postings
-  end
-end
