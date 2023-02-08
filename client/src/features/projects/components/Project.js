655cfe182f6a359af1fde6f35e4a8a7f071384c0
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-07 21:43:11 -0700

    Standardized logic to see if the project is in the past

--- client/src/features/projects/components/Project.js
+++ client/src/features/projects/components/Project.js
@@ -2,24 +2,24 @@ import React from "react";
 import { useSystemMode } from "../../../SystemModeContext";
 import moment from "moment";
 export default function Project({ project, onEdit, onDelete }) {
-  
+  const is_in_past = Date.parse(project.due_date) < Date.parse(new Date())
   const systemMode = useSystemMode();
   return(
     <div class="card text-center w-50 d-flex mx-auto mb-4">
       <div
         class={`card-header fw-bold text-uppercase text-white colors-${systemMode.toLowerCase()}`}
       >
-        {Date.parse(project.due_date) < Date.parse(new Date()) ? "Completed" : "In Progress"}
+        {is_in_past ? "Completed" : "In Progress"}
       </div>
       <div class="card-body d-flex flex-column">
         <p class="card-text">
-          Working with {systemMode === "Freelancer" ? project.buyer_username : project.freelancer_username}
+          {is_in_past ? "Worked" : "Working"} with {systemMode === "Freelancer" ? project.buyer_username : project.freelancer_username}
         </p>
         <p class="card-text">
           {systemMode === "Freelancer" ? "Price" : "Cost"} - ${project.cost.toLocaleString("en-US")}
         </p>
-        <p class="card-text">
-          {Date.parse(project.due_date) < Date.parse(new Date()) ? "Finished" : "Due"}: {moment(project.due_date).format("MM/DD/YYYY HH:MM")}
+        <p class="card-text fw-bold">
+          {is_in_past ? "Finished" : "Due"}: {moment(project.due_date).format("MM/DD/YYYY HH:MM")}
         </p>
       </div>
       <div class="btn-group card-footer text-muted">
