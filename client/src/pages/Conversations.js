06a852bb0c51663096ed9a500e35bcd3c07538bd
Andrew Stahl <andrewstahl96@gmail.com>
2023-02-08 11:34:34 -0700

    Renamed directory

--- client/src/pages/Conversations.js
+++ client/src/pages/Conversations.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ConversationsIndex from "../features/conversationstemp/ConversationsIndex";
+import ConversationsIndex from "../features/conversations/ConversationsIndex";
 
 export default function Conversations() {
   return <ConversationsIndex />;
